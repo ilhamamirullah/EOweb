eedc8370b65358a5ae6cd8c9c3398fbf8ca2a750
yandrapfss <budiantoyandra@gmail.com>
2018-01-06 23:50:12 +0700

    editing template

--- application/controllers/debindo.php
+++ application/controllers/debindo.php
@@ -5,6 +5,22 @@ class debindo extends CI_Controller {
 
 	public function index()
 	{
+    $this->load->view('templates/header');
 		$this->load->view('index');
+    $this->load->view('templates/footer');
 	}
+
+  public function myclient()
+  {
+    $this->load->view('templates/header');
+    $this->load->view('pages/myclient');
+    $this->load->view('templates/footer');
+  }
+
+  public function company()
+  {
+    $this->load->view('templates/header');
+    $this->load->view('pages/company');
+    $this->load->view('templates/footer');
+  }
 }
--- application/views/pages/company.php
+++ application/views/pages/company.php
@@ -5,8 +5,7 @@
     <!-- Content Header (Page header) -->
     <section class="content-header">
       <h1>
-        Data Tables
-        <small>advanced tables</small>
+        Company List
       </h1>
       <ol class="breadcrumb">
         <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
